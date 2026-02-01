2cb64aab55a5ff6f22d37c3ecc5a9cc22940e44f
dextermayhewjd <dexterding99@icloud.com>
2026-02-02 03:07:13 +0800

    smoke test ready

---
+++ build/CMakeFiles/minimal_generate.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minimal_generate.dir/examples/minimal_generate.cc.o"
+  "CMakeFiles/minimal_generate.dir/examples/minimal_generate.cc.o.d"
+  "minimal_generate"
+  "minimal_generate.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/minimal_generate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ examples/minimal_generate.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+    std::cout<<"S00 smoke test: build works.\n"<<std::endl;
+    return 0;
+}
