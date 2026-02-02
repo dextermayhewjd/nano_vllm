a378c61c4574df74324189ef908d1a2994498e09
dextermayhewjd <dexterding99@icloud.com>
2026-02-03 01:44:51 +0800

    include sucessfully

--- examples/minimal_generate.cc
+++ examples/minimal_generate.cc
@@ -1,6 +1,8 @@
 #include <iostream>
+#include "llm/api/engine.h"
 
 int main(){
+    llm::api::Engine engine;
     std::cout<<"S00 smoke test: build works.\n"<<std::endl;
     return 0;
 }
---
+++ include/llm/api/engine.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace llm::api{
+
+
+    class Engine{
+
+    };
+
+} // namespace llm:api
